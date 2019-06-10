cbd77555438f367dc0e089337f5dde0f0ae180d0
ahmedbank <40931437+ahmedbank@users.noreply.github.com>
2019-06-10 16:38:56 +0200

    add design book currency licenses

---
+++ ECBNewWeb/ECBNewWeb/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECBNewWeb.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult AddBook()
+        {
+            return View();
+        }
+
+
+        public ActionResult AddReceiptType()
+        {
+            return View();
+        }
+
+        public ActionResult HandleBookReceipts()
+        {
+            return View();
+        }
+    }
+}
---
+++ ECBNewWeb/ECBNewWeb/Controllers/CurrencyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECBNewWeb.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        // GET: Currency
+        public ActionResult AddCurrency()
+        {
+            return View();
+        }
+
+        public ActionResult CurrenyConversion()
+        {
+            return View();
+        }
+    }
+}
