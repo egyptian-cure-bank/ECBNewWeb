80fb4a708a500816009940d6229473e9fd59ada7
Mohamed Abd Allah <m.abdallah@EFB.Local>
2019-06-17 09:56:44 +0200

    Book done except responsibility

---
+++ ECBNewWeb/ECBNewWeb/Models/BookModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECBNewWeb.Models
+{
+    public class BookModel
+    {
+        public int BookTypeId { get; set; }
+        [Range(1,99999999999999,ErrorMessage ="الرقم لايمكن ان يكون صفر")]
+        public int BookNo { get; set; }
+        [Required(AllowEmptyStrings = false,ErrorMessage ="يجب إختيار إسم دفتر")]
+        public int RecTypeId { get; set; }
+        public string RecTypeName { get; set; }
+        public IEnumerable<SelectListItem> MyRecTypes { get; set; }
+        [Required]
+        public int LicenseId { get; set; }
+        public int? Active { get; set; }
+        public bool IsActive {
+            get { return Active == 1; }
+            set { Active = value ? 1 : 0; }
+        }
+        [Range(1, 99999999999999, ErrorMessage = "الرقم لايمكن ان يكون صفر")]
+        [Required]
+        public int? FirstReceiptNo { get; set; }
+        [Range(1, 99999999999999, ErrorMessage = "الرقم لايمكن ان يكون صفر")]
+        [Required]
+        public int?LastReceiptNo { get; set; }
+    }
+}
---
+++ ECBNewWeb/ECBNewWeb/Models/ReceiptTypeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace ECBNewWeb.Models
+{
+    public class ReceiptTypeModel
+    {
+        [Required(AllowEmptyStrings =false,ErrorMessage ="برجاء كتابة إسم الدفتر")]
+        [Remote("CheckReceiptTypeNameDuplication", "Book", ErrorMessage = "نوع الدفتر موجود بالفعل")]
+        public string ReceiptTypeName { get; set; }
+        public int Active { get; set; }
+    }
+}
