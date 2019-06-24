c335b5ebe3adee71b9bb9aad1b7f3a56929d0e83
Mohamed Abd Allah <m.abdallah@EFB.Local>
2019-06-24 13:57:10 +0200

    Cheque Information is add but still needs validation tunning

---
+++ ECBNewWeb/ECBNewWeb/CustomValidation/BankInfoChequeBankIdValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECBNewWeb.CustomValidation
+{
+    public class BankInfoChequeBankIdValidator : ValidationAttribute
+    {
+        private readonly string _ComarisonProperty;
+        public BankInfoChequeBankIdValidator(string CompraisonProberty)
+        {
+            _ComarisonProperty = CompraisonProberty;
+        }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var property = validationContext.ObjectType.GetProperty(_ComarisonProperty);
+            if (property == null)
+                throw new ArgumentException("Property with this name not found");
+            var ComparisonValue = property.GetValue(validationContext.ObjectInstance);
+            if (ComparisonValue.ToString() == "true")
+            {
+                if (Convert.ToInt32(value) == 0)
+                {
+                    return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
+                }
+            }
+            return ValidationResult.Success;
+            //return base.IsValid(value, validationContext);
+        }
+    }
+}
