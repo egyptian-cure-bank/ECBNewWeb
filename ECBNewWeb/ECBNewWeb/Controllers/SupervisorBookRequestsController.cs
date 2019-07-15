3ac9501b438aa104d6d0b6c5c32a3aac06436d0f
Mohamed Abd Allah <m.abdallah@EFB.Local>
2019-07-15 11:48:12 +0200

    Supervisor Book requests is done and bug fixed in finance book requests

--- ECBNewWeb/ECBNewWeb/Controllers/SupervisorBookRequestsController.cs
+++ ECBNewWeb/ECBNewWeb/Controllers/SupervisorBookRequestsController.cs
@@ -14,11 +14,11 @@ using System.Web.Security;
 
 namespace ECBNewWeb.Controllers
 {
-    public class FinanceBookRequestsController : Controller
+    public class SupervisorBookRequestsController : Controller
     {
         private CustomMembershipUser UserInfo;
-        // GET: FinanceBookRequests
-        public ActionResult BookRequests()
+        // GET: SupervisorBookRequests
+        public ActionResult SupervisorApproveBookRequest()
         {
             if (User.Identity.IsAuthenticated)
             {
@@ -37,14 +37,14 @@ namespace ECBNewWeb.Controllers
                 List<BookRequestModel> MyRequests = (from S in db.BookRequests
                                                      join D in db.BookRequestDetails on S.RequestNo equals D.RequestNo
                                                      join E in db.Employees on S.EmployeeId equals E.EmployeeId
-                                                     where S.Active == 1 && D.FinanceApproval != 1 && D.SupervisorApproval == 1
-                                               //&& S.EmployeeId == UserInfo.EmployeeId
-                                               select new BookRequestModel() { RequestId = S.RequestId, RequestNo = S.RequestNo, EmployeeNo = E.EmployeeNo }).OrderByDescending(order => order.RequestNo).Distinct().ToList<BookRequestModel>();
+                                                     where S.Active == 1 && D.FinanceApproval != 1 && D.SupervisorApproval != 1
+                                                     && E.ParentEmployeeId == UserInfo.EmployeeId
+                                                     select new BookRequestModel() { RequestId = S.RequestId, RequestNo = S.RequestNo,EmployeeNo = E.EmployeeNo }).OrderByDescending(order => order.RequestNo).Distinct().ToList<BookRequestModel>();
                 foreach (BookRequestModel item in MyRequests)
                 {
                     SelectListItem selectList = new SelectListItem()
                     {
-                        Text = item.EmployeeNo.ToString()+"-"+item.RequestNo.ToString(),
+                        Text = item.EmployeeNo.ToString()+"-"+ item.RequestNo.ToString(),
                         Value = item.RequestId.ToString()
                     };
                     Items.Add(selectList);
@@ -52,7 +52,7 @@ namespace ECBNewWeb.Controllers
             }
             return Items;
         }
-        public JsonResult GetRequestDetails (long RequestId)
+        public JsonResult GetRequestDetails(long RequestId)
         {
             using (MarketEntities db = new MarketEntities())
             {
@@ -60,36 +60,33 @@ namespace ECBNewWeb.Controllers
                                                join d in db.BookRequestDetails on h.RequestNo equals d.RequestNo
                                                join e in db.Employees on h.EmployeeId equals e.EmployeeId
                                                join e2 in db.Employees on e.ParentEmployeeId equals e2.EmployeeId
-                                               into p from parentemp in p.DefaultIfEmpty()
+                                               into p
+                                               from parentemp in p.DefaultIfEmpty()
                                                join u in db.UserLogins on e.EmployeeId equals u.employee_id
                                                join s in db.UserSites on u.id equals s.UserId
                                                join t in db.marketingsites on s.SiteId equals t.id
                                                join r in db.marketingrectypes on d.ReceiptTypeId equals r.id
-                                               where h.Active == 1 && d.SupervisorApproval == 1 && d.FinanceApproval != 1 && h.RequestId == RequestId
-                                               select new BookRequestModel {RequestNo = h.RequestNo, SiteName = t.sitename,EmployeeId = e.EmployeeId,
-                                               FullEmployeeName = e.FirstName+" "+e.MiddleName+" "+e.LastName,
-                                               RecTypeId = d.ReceiptTypeId,BookTypeName = r.name,Amount = d.Amount,
-                                                   SupervisorName = parentemp.FirstName+" "+parentemp.MiddleName+" "+parentemp.LastName}
+                                               where h.Active == 1 && d.SupervisorApproval != 1 && d.FinanceApproval != 1 && h.RequestId == RequestId
+                                               select new BookRequestModel
+                                               {
+                                                   RequestNo = h.RequestNo,
+                                                   SiteName = t.sitename,
+                                                   EmployeeId = e.EmployeeId,
+                                                   FullEmployeeName = e.FirstName + " " + e.MiddleName + " " + e.LastName,
+                                                   RecTypeId = d.ReceiptTypeId,
+                                                   BookTypeName = r.name,
+                                                   Amount = d.Amount,
+                                                   SupervisorName = parentemp.FirstName + " " + parentemp.MiddleName + " " + parentemp.LastName
+                                               }
                                                ).ToList<BookRequestModel>();
                 return Json(Data, JsonRequestBehavior.AllowGet);
             }
         }
-        public JsonResult GetRequestMetaData (int[]RecTypeId,int[] Amount)
+        public JsonResult GetRequestMetaData(int[] RecTypeId, int[] Amount)
         {
             DataTable dt = new DataTable();
             SqlDataAdapter adapt = null;
             string JsonString = null;
-            //string Cmd = "Select BookNo,HandleBookReceipts.BookReceiptId,BookTypes.RecTypeId,marketingrectype.[name],HandleBookReceipts.FirstReceiptNo,HandleBookReceipts.LastReceiptNo " +
-            //            "FROM BookTypes " +
-            //            "Inner Join HandleBookReceipts " +
-            //            "On BookTypes.BookTypeId = HandleBookReceipts.BookTypeId " +
-            //            "Inner Join marketingrectype "+
-            //            "On BookTypes.RecTypeId = marketingrectype.id "+
-            //            "Where Not Exists(Select 1 From BookResposibilities Where dbo.BookResposibilities.HandleBookReceiptId = dbo.HandleBookReceipts.BookReceiptId) " +
-            //            "And BookTypes.RecTypeId = @BookTypeId " +
-            //            "Order By marketingrectype.[name] " +
-            //            "OFFSET 0 Rows " +
-            //            "Fetch First @Amount Rows only";
             using (SqlConnection Conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ECBConnectionString"].ConnectionString))
             {
                 Conn.Open();
@@ -110,19 +107,16 @@ namespace ECBNewWeb.Controllers
                             " Order By marketingrectype.[name] " +
                             "OFFSET 0 Rows " +
                             "Fetch First " + Amount[i] + " Rows only";
-                            //Command.Parameters["@BookTypeId"].Value = BookTypeId[i];
-                            //Command.Parameters["@Amount"].Value = Amount[i];                            
                             adapt = new SqlDataAdapter(Command);
                             adapt.Fill(dt);
                         }
                     }
                     JsonSerializerSettings SerSettings = new JsonSerializerSettings();
-                        SerSettings.Culture = System.Globalization.CultureInfo.InstalledUICulture;
-                        JsonString = JsonConvert.SerializeObject(dt, SerSettings);
+                    SerSettings.Culture = System.Globalization.CultureInfo.InstalledUICulture;
+                    JsonString = JsonConvert.SerializeObject(dt, SerSettings);
                 }
             }
             return Json(JsonString, JsonRequestBehavior.AllowGet);
-
         }
         [HttpPost]
         public ActionResult ApproveBookRequest(BookRequestModel Model)
@@ -130,24 +124,15 @@ namespace ECBNewWeb.Controllers
             TempData["Msg"] = "";
             int InsertedRows = 0;
             int UpdatedRows = 0;
-            string Cmd = "Insert Into BookResposibilities "+
-                        "(EmployeeId, HandleBookReceiptId, ReceiveDate, DoneFlag)Values(@EmployeeId, @HandleBookReceiptId, @ReceiveDate, 0)";
-
-            string UpdateCmd = "Update BookRequestDetails Set FinanceApproval = 1 Where RequestNo = (Select RequestNo From BookRequests " +
+            string UpdateCmd = "Update BookRequestDetails Set SupervisorApproval = 1 Where RequestNo = (Select RequestNo From BookRequests "+
                                 "Where RequestId = @RequestId) " +
                                 "And ReceiptTypeId = @RecTypeId";
-            if (Model.HandleBookReceiptId != 0)
+            using (SqlConnection Conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ECBConnectionString"].ConnectionString))
             {
-                using (SqlConnection Conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ECBConnectionString"].ConnectionString))
+                Conn.Open();
+                if (Model.RecTypeId != null)
                 {
-                    Conn.Open();
-                    using (SqlCommand Com = new SqlCommand(Cmd, Conn))
-                    {
-                        Com.Parameters.AddWithValue("@EmployeeId", Model.EmployeeId);
-                        Com.Parameters.AddWithValue("@HandleBookReceiptId", Model.HandleBookReceiptId);
-                        Com.Parameters.AddWithValue("@ReceiveDate", DateTime.Now.Date);
-                        InsertedRows += Com.ExecuteNonQuery();
-                    }
+
                     using (SqlCommand ComUpdate = new SqlCommand(UpdateCmd, Conn))
                     {
                         ComUpdate.Parameters.AddWithValue("@RequestId", Model.RequestId);
@@ -156,7 +141,7 @@ namespace ECBNewWeb.Controllers
                     }
                 }
             }
-            if (InsertedRows > 0 || UpdatedRows > 0)
+            if (InsertedRows >= 0 || UpdatedRows >= 0)
             {
                 TempData["Msg"] = "تم الحفظ بنجاح";
             }
@@ -164,11 +149,7 @@ namespace ECBNewWeb.Controllers
             {
                 TempData["Msg"] = "لم يتم الحفظ";
             }
-            return RedirectToAction("BookRequests", TempData["Msg"]);
-        }
-        private void MarkRequestAsApproved(long RequestNo, int RecTypeId)
-        {
-
+            return RedirectToAction("SupervisorApproveBookRequest", TempData["Msg"]);
         }
     }
 }
